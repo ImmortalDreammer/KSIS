1f49451ebcf8a6f7a2c1bf851fc0187bd46b1f8f
john sutray <johnsutray@gmail.com>
2019-05-25 18:31:17 +0300

    It works. Don't touch ))

---
+++ Client/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.IO;
+using System.Net.Sockets;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            do
+            {
+                Console.Write("Enter addres: ");
+                string str = Console.ReadLine();
+                if (str.Equals("exit"))
+                    return;
+                Uri uri = new Uri(str);
+
+                Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                socket.Connect(uri.Host, uri.Port);
+
+                string reqStr = $"GET {uri.AbsolutePath} HTTP/1.1\r\n";
+                string host = $"Host: {uri.Host}\r\n";
+                string contentLength = "Content-Length: 0\r\n";
+                string userAgent = "User-Agent: chrome\r\n";
+                string acceptLanguage = "Accepted-Language: en/us\r\n";
+                string fullRequestString = reqStr + userAgent + contentLength + host + acceptLanguage + "\r\n\r\n";
+                byte[] req = Encoding.ASCII.GetBytes(fullRequestString);
+                Console.WriteLine(fullRequestString);
+
+                socket.Send(req);
+
+                byte[] response = new byte[256];
+                StringBuilder builder = new StringBuilder();
+                int bytes = 0;
+                do
+                {
+                    bytes = socket.Receive(response, response.Length, 0);
+                    builder.Append(Encoding.ASCII.GetString(response, 0, bytes));
+                } while (socket.Available > 0);
+
+                Console.WriteLine(builder.ToString());
+                socket.Shutdown(SocketShutdown.Both);
+                socket.Close();
+            } while (true);
+        }
+    }
+}
---
+++ Server/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Net;
+using Flurl.Util;
+
+namespace lab_8_1_server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var port = 3000;
+            var rootDir = "wwwroot";
+            var server = new HttpLabServer(port,rootDir);
+            server.Initialize();
+            server.Start();
+        }
+    }
+
+    public class HttpLabServer
+    {
+        private readonly string _rootDirectory;
+        private readonly int _port;
+        private HttpListener _httpListener;
+
+        public HttpLabServer(int port, string rootDirectory)
+        {
+            _rootDirectory = rootDirectory;
+            _port = port;
+        }
+
+        public void Initialize()
+        {
+            _httpListener = new HttpListener();
+            _httpListener.Prefixes.Add($"http://localhost:{_port}/");
+            _httpListener.Start();
+        }
+
+        private string GetFilePath(Uri uri)
+        {
+            var path = uri.LocalPath.Replace("/", "\\").Trim('\\');
+            return Path.Combine(Directory.GetCurrentDirectory(), _rootDirectory, path);
+        }
+
+        public void Start()
+        {
+            while (true)
+            {
+                var context = _httpListener.GetContext();
+                var filePath = GetFilePath(context.Request.Url);
+
+                if (!File.Exists(filePath))
+                {
+                    context.Response.StatusCode = 404;
+                    LogContext(context);
+                    context.Response.Close();
+                    continue;
+                }
+
+                using (var fileStream = new FileStream(filePath, FileMode.Open, FileAccess.Read))
+                using (var fileReader = new StreamReader(fileStream))
+                using (var outputStreamWriter = new StreamWriter(context.Response.OutputStream))
+                {
+                    outputStreamWriter.Write(fileReader.ReadToEnd());
+                }
+
+                context.Response.StatusCode = 200;
+                LogContext(context);
+                context.Response.Close();
+            }
+        }
+
+        public void LogContext(HttpListenerContext context)
+        {
+            Console.WriteLine($"<--- {context.Request.HttpMethod} {context.Request.Url.LocalPath}");
+            context.Request.Headers.AllKeys.ToList().ForEach(key =>
+            {
+                Console.WriteLine($"\t{key,15}: {context.Request.Headers[key],15}");
+            });
+            Console.WriteLine($"---> {context.Request.HttpMethod} {context.Request.Url.LocalPath} {context.Response.StatusCode}");
+            context.Response.Headers.AllKeys.ToList().ForEach(key =>
+            {
+                Console.WriteLine($"\t{key,15}: {context.Request.Headers[key],15}");
+            });
+        }
+    }
+}
